6a5b0391a385dade9f6d298f297b1a682686bc09
Daniel Sinderson <drsinderson@gmail.com>
2024-07-30 14:47:36 -0700

    began exploratory analysis

--- 0 - Data Science/Kickstarter Project/exploratory_analysis.r
+++ 0 - Data Science/Kickstarter Project/exploratory_analysis.r
@@ -1,4 +1,26 @@
 library(tidyverse)
 
-ks1 <- read_tsv("0 - Data Science/38050-0001-Data.tsv")
-glimpse(ks1)
+ks <- read_tsv("0 - Data Science/Kickstarter Project/38050-0001-Data.tsv")
+glimpse(ks)
+
+
+# find games and convert the USD variables to numeric values
+games <- ks |>
+  filter(CATEGORY == 12) |>
+  mutate(
+    GOAL_IN_USD = as.numeric(str_remove_all(str_sub(GOAL_IN_USD, 2, -1), ",")),
+    PLEDGED_IN_USD = as.numeric(str_remove_all(str_sub(PLEDGED_IN_USD, 2, -1), ",")),
+  )
+glimpse(games)
+
+
+
+# there are some extreme outliers in goal
+games |>
+  ggplot(aes(x = STATE, y = GOAL_IN_USD)) +
+  geom_boxplot()
+
+games |>
+  ggplot(aes(x = GOAL_IN_USD)) +
+  geom_density() +
+  scale_x_log10()
